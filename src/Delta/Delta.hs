b37fb896765d7269351ee6f7aba141686b0377d7
Trevor Cook <trevor.j.cook@gmail.com>
2019-05-30 08:23:41 -0400

    Returned to basing DeltaOf on the atomic delta and specifying a return type. *  Currently No patch

--- src/Delta/Delta.hs
+++ src/Delta/Delta.hs
@@ -12,58 +12,89 @@ data Delta a
 
 class HasDelta a where
   type DeltaOf a
-  calcDeltaOf    :: a -> a -> DeltaOf a
-  default calcDeltaOf :: ( HasDeltaVia (DeltaCtx a) a (DeltaOf a) ) =>
-     a -> a -> DeltaOf a
-  calcDeltaOf = calcDeltaVia @(DeltaCtx a)
+  type DeltaReturnType a :: DeltaReturnKind
+  type DeltaReturnType a = CalcDeltaReturnType a
+  calcDeltaOf    :: a -> a -> DeltaReturn a
+  default calcDeltaOf ::
+    HasDeltaVia (DeltaReturnType a) a (DeltaReturn a) =>
+     a -> a -> DeltaReturn a
+  calcDeltaOf = calcDeltaVia @(DeltaReturnType a) @a @(DeltaReturn a)
 
-type DeltaCtx a = DeltaCtx_ (DeltaOf a)
-type family DeltaCtx_ (dlt :: * ) :: * -> * where
-  DeltaCtx_ (Revise a b) = Revise a
-  DeltaCtx_ (Change (Revise a b)) =  Compose Change (Revise a)
-  DeltaCtx_ (Change c) =  Change
-  DeltaCtx_ x          = Identity
+data DeltaReturnKind = DeltaAlways | DeltaMaybe
+type DeltaReturn a = DeltaReturn_ a (DeltaReturnType a)
+type family DeltaReturn_ a (t::DeltaReturnKind) where
+  DeltaReturn_ a DeltaAlways = DeltaOf a
+  DeltaReturn_ a DeltaMaybe = Maybe (DeltaOf a)
+type CalcDeltaReturnType a = CalcDeltaReturnType_ a (IsSumRep (Rep a))
+type family CalcDeltaReturnType_ a (b::Bool) :: DeltaReturnKind where
+  CalcDeltaReturnType_ a True = DeltaMaybe
+  CalcDeltaReturnType_ a False = DeltaAlways
+type family IsSumRep a :: Bool where
+  IsSumRep (M1 t x (a :+: b)) = True
+  IsSumRep x                  = False
 
-class HasDeltaVia (ctx :: * -> *) a d where
+-- class HasPatch a where
+--   calcPatchOf :: a -> a -> Revise a (DeltaOf a)
+-- instance HasPatchVia (DeltaCtx a) a => HasPatch a where
+--   calcPatchOf = calcPatchVia @(DeltaCtx a)
+-- class HasDelta a => HasPatchVia ctx a where
+--   calcPatchVia :: a -> a -> Revise a (DeltaOf a)
+-- instance (HasDelta a, DeltaCtx a ~ Maybe) =>
+--   HasPatchVia Maybe a where
+--   calcPatchVia a a' = maybe (Replace a') Update
+--                     $ calcDeltaOf a a'
+-- instance (HasDelta a, DeltaCtx a ~ Identity) => HasPatchVia Identity a where
+--   calcPatchVia a = Update . calcDeltaOf a
+--
+-- type DeltaCtx a = DeltaCtx_ (DeltaOf a)
+-- type family DeltaCtx_ (dlt :: * ) :: * -> * where
+--   DeltaCtx_ (Revise a b) = Revise a
+--   DeltaCtx_ (Change (Revise a b)) =  Compose Change (Revise a)
+--   DeltaCtx_ (Change c) =  Change
+--   DeltaCtx_ x          = Identity
+
+class HasDeltaVia (ctx :: DeltaReturnKind) a d where
   calcDeltaVia :: a -> a -> d
 
-instance  ( Generic a, Generic d
-          , HasDeltaGen SigChange (Rep a) (Rep d)) =>
-  HasDeltaVia Identity a d where
-  calcDeltaVia a a' = sigChange to to
-                    $ calcDeltaGen @SigChange (from a) (from a')
+instance                            ( Generic a, Generic d
+                                    , HasDeltaGen Freshest (Rep a) (Rep d)) =>
+  HasDeltaVia  DeltaAlways a (Change d) where
+  calcDeltaVia a a' = freshest (const Unchanged) (Changed . to)
+                    $ calcDeltaGen @Freshest (from a) (from a')
+instance {-#Overlappable#-}         ( Generic a, Generic d
+                                    , HasDeltaGen Freshest (Rep a) (Rep d)) =>
+  HasDeltaVia DeltaAlways a d where
+  calcDeltaVia a a' = freshest to to
+                    $ calcDeltaGen @Freshest (from a) (from a')
 
 instance  ( Generic a, Generic d
-          , HasDeltaGen (Compose Maybe SigChange) (Rep a) (Rep d)) =>
-  HasDeltaVia  (Compose Change (Revise a)) a (Change (Revise a d)) where
-  calcDeltaVia a a' = maybe (Changed $ Replace a')
-                            (sigChange (const Unchanged)
-                                       (Changed . Update . to))
+          , HasDeltaGen (Compose Maybe Freshest) (Rep a) (Rep d)) =>
+  HasDeltaVia DeltaMaybe a (Maybe (Change d)) where
+  calcDeltaVia a a' = fmap (freshest (const Unchanged) (Changed . to))
                     . getCompose
-                    $ calcDeltaGen @(Compose Maybe SigChange) (from a) (from a')
-instance  ( Generic a, Generic d
-          , HasDeltaGen (Compose Maybe SigChange) (Rep a) (Rep d)) =>
-  HasDeltaVia  (Revise a) a (Revise a d) where
-  calcDeltaVia a a' = maybe (Replace a')
-                            (sigChange (Update . to)
-                                       (Update . to))
+                    $ calcDeltaGen @(Compose Maybe Freshest) (from a) (from a')
+instance {-#Overlappable#-}         ( Generic a, Generic d
+                  , HasDeltaGen (Compose Maybe Freshest) (Rep a) (Rep d)) =>
+  HasDeltaVia DeltaMaybe a (Maybe d) where
+  calcDeltaVia a a' = fmap (freshest to to)
                     . getCompose
-                    $ calcDeltaGen @(Compose Maybe SigChange) (from a) (from a')
+                    $ calcDeltaGen @(Compose Maybe Freshest) (from a) (from a')
 
 
 
 
-data SigChange a = SigUnchanged a | SigChanged a
+data Freshest a = Older a | Newer a
  deriving Functor
-instance Applicative SigChange where
-  pure = SigUnchanged
-  (SigUnchanged f) <*> (SigUnchanged a) = SigUnchanged (f a)
-  (SigUnchanged f) <*> (SigChanged a) = SigChanged (f a)
-  (SigChanged f) <*> (SigUnchanged a) = SigChanged (f a)
-  (SigChanged f) <*> (SigChanged a) = SigChanged (f a)
-sigChange :: (a -> b) -> (a -> b) -> SigChange a -> b
-sigChange u _ (SigUnchanged a) = u a
-sigChange _ c (SigChanged a)   = c a
+instance Applicative Freshest where
+  pure = Older
+  (Older f) <*> (Older a) = Older (f a)
+  (Older f) <*> (Newer a) = Newer (f a)
+  (Newer f) <*> (Older a) = Newer (f a)
+  (Newer f) <*> (Newer a) = Newer (f a)
+freshest :: (a -> b) -> (a -> b) -> Freshest a -> b
+freshest u _ (Older a) = u a
+freshest _ c (Newer a)   = c a
+
 
 
 class HasDeltaGen ctx a d where
@@ -73,17 +104,17 @@ class HasDeltaGen ctx a d where
 instance               (Functor f, HasDeltaGen f a d) =>
   HasDeltaGen f (M1 t x a) (M1 t x d) where
   calcDeltaGen a a' = M1 <$> calcDeltaGen @f (unM1 a) (unM1 a')
-instance               (HasDelta a, DeltaOf a ~ Change d) =>
-  HasDeltaGen SigChange (K1 x a) (K1 x (Change d)) where
-  calcDeltaGen a a' = change (SigUnchanged $ K1 Unchanged)
-                             (SigChanged . K1. Changed)
+instance               (HasDelta a, DeltaReturn a ~ Change d) =>
+  HasDeltaGen Freshest (K1 x a) (K1 x (Change d)) where
+  calcDeltaGen a a' = change (Older $ K1 Unchanged)
+                             (Newer . K1. Changed)
                     $ calcDeltaOf (unK1 a) (unK1 a')
 instance
-  HasDeltaGen SigChange (K1 x a) (K1 x (Static a)) where
-  calcDeltaGen a a' = SigUnchanged $ K1 Static
-instance  {-#Overlappable#-}        (HasDelta a, DeltaOf a ~ d) =>
-  HasDeltaGen SigChange (K1 x a) (K1 x d) where
-  calcDeltaGen a a' = SigChanged . K1
+  HasDeltaGen Freshest (K1 x a) (K1 x (Static a)) where
+  calcDeltaGen a a' = Older $ K1 Static
+instance  {-#Overlappable#-}        (HasDelta a, DeltaReturn a ~ d) =>
+  HasDeltaGen Freshest (K1 x a) (K1 x d) where
+  calcDeltaGen a a' = Newer . K1
                     $ calcDeltaOf (unK1 a) (unK1 a')
 instance                ( HasDeltaGen ctx al dl
                         , HasDeltaGen ctx ar dr
@@ -93,14 +124,14 @@ instance                ( HasDeltaGen ctx al dl
     = (:*:)
       <$> calcDeltaGen @ctx al al'
       <*> calcDeltaGen @ctx ar ar'
-instance                ( HasDeltaGen SigChange al dl
-                        , HasDeltaGen SigChange ar dr
+instance                ( HasDeltaGen Freshest al dl
+                        , HasDeltaGen Freshest ar dr
                         ) =>
-  HasDeltaGen (Compose Maybe SigChange) (al :+: ar) (dl :+: dr) where
+  HasDeltaGen (Compose Maybe Freshest) (al :+: ar) (dl :+: dr) where
   calcDeltaGen (L1 a) (L1 a') = Compose . Just
-                              $ L1 <$> calcDeltaGen @SigChange a a'
+                              $ L1 <$> calcDeltaGen @Freshest a a'
   calcDeltaGen (R1 a) (R1 a') = Compose . Just
-                              $ R1 <$> calcDeltaGen @SigChange a a'
+                              $ R1 <$> calcDeltaGen @Freshest a a'
   calcDeltaGen _       _      = Compose Nothing
 
 
@@ -116,9 +147,11 @@ calcDeltaOfNEQ a b | a == b = Unchanged
                    | otherwise = Changed b
 instance HasDelta Bool where
   type DeltaOf Bool = Change Bool
+  type DeltaReturnType Bool = DeltaAlways
   calcDeltaOf = calcDeltaOfNEQ
 instance HasDelta Int where
   type DeltaOf Int = Change Int
+  type DeltaReturnType Int = DeltaAlways
   calcDeltaOf = calcDeltaOfNEQ
 -- instance HasDelta Double where
 --   type DeltaOf Double = ()
@@ -129,4 +162,5 @@ instance HasDelta Int where
 --   calcDeltaOf = calcDeltaOfNEQ
 instance HasDelta String where
   type DeltaOf String = Change String
+  type DeltaReturnType String = DeltaAlways
   calcDeltaOf = calcDeltaOfNEQ
--- src/Test.hs
+++ src/Test.hs
@@ -17,23 +17,23 @@ type family Point (k :: DeltaPointKind) (f :: * -> *) (a :: *) :: * where
 
 type family PointInDelta (k :: DeltaPointKind) (a :: * ):: * where
   PointInDelta StaticPointType a = Static a
-  PointInDelta DeltaPointType  a = DeltaOf a
+  PointInDelta DeltaPointType  a = DeltaReturn a
 
 type DeltaPoint f a = Point DeltaPointType f a
 type StaticPoint f a = Point StaticPointType f a
 
 type A = A' Z
-data A' f = A (DeltaPoint f Int) (DeltaPoint f Int)
+data A' f = A (DeltaPoint f Int) (DeltaPoint f A)
           | Ay (StaticPoint f Int)
           deriving Generic
 instance HasDelta A where
-  type DeltaOf A = Change (Revise A (A' Delta))
+  type DeltaOf A = Change (A' Delta)
 
 deriving instance (Constraints (A' f) Show) => Show (A' f)
 a1,a2
  ,ay1,ay2
  :: A
-a1 = A 1 2
-a2 = A 2 2
+a1 = A 1 ay1
+a2 = A 2 ay1
 ay1 = Ay 1
 ay2 = Ay 2
